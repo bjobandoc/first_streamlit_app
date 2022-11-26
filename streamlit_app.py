8234ca5ad0e7642fbb7e6a889d231d0e733aaa10
Bryan Obando Castillo <75287770+bjobandoc@users.noreply.github.com>
2022-11-25 23:33:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ try:
    if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
    else:
-        back_from_funtion = get_fruityvice_data(fruit_choice)
+        back_from_function = get_fruityvice_data(fruit_choice)
         #import requests
         streamlit.dataframe(fruityvice_normalized)
        
