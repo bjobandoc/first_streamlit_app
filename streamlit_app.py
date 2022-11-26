83847439ae63d34b655ac79595099c3efdd0063d
Bryan Obando Castillo <75287770+bjobandoc@users.noreply.github.com>
2022-11-25 23:48:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,12 +46,12 @@ except URLError as e:
 streamlit.header("The fruit loas list contains:")
 #snowflake-releted functions
 def get_fruit_load_list():
-     with my_cnx.cursor() as my_cur:
-          my_cur.execute("select * from fruit_load_list")
-          return my_cur.fetchall()
+   with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+        return my_cur.fetchall()
       
 #about a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
-    my_cnz = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
