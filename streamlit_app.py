5f72080ff4ee092992fe0ac1c75458e3e145a71b
Bryan Obando Castillo <75287770+bjobandoc@users.noreply.github.com>
2022-11-25 23:42:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def get_fruit_load_list():
          return my_cur.fetchall()
       
 #about a button to load the fruit
-if streamlit.button('Get Fruit Load List')
+if streamlit.button('Get Fruit Load List'):
    my_cnz = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
